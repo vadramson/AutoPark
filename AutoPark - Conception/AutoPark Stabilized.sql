96c16b13e463e4ea90631a5038f8cada2c304ee9
Vadramson <vadramson@gmail.com>
2019-05-12 00:03:29 +0100

    Conception completed. Database generated and imported into WAMP. Created default user using WAMP and Exported current database with default user. Everything set for the next stages of implementation of application.

---
+++ AutoPark - Conception/AutoPark Stabilized.sql
@@ -0,0 +1,152 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     11/05/2019 23:28:15                          */
+/*==============================================================*/
+
+
+/*==============================================================*/
+/* Table: CarModel                                              */
+/*==============================================================*/
+create table CarModel
+(
+   idModel              int not null AUTO_INCREMENT,
+   model                varchar(254),
+   year                 int,
+   created              datetime,
+   updated               datetime,
+   mark                 varchar(254),
+   status               boolean,
+   primary key (idModel)
+);
+
+/*==============================================================*/
+/* Table: Drivers                                               */
+/*==============================================================*/
+create table Drivers
+(
+   idDriver             int not null AUTO_INCREMENT,
+   idUser               int,
+   name                 varchar(254),
+   availability         boolean,
+   licenseType          varchar(254),
+   licenseID            varchar(254),
+   picture              varchar(254),
+   idNumber             varchar(254),
+   email                varchar(254),
+   phone                varchar(254),
+   created              datetime,
+   updated               datetime,
+   status               boolean,
+   primary key (idDriver)
+);
+
+/*==============================================================*/
+/* Table: Expenditure                                           */
+/*==============================================================*/
+create table Expenditure
+(
+   idExpenditure        int not null AUTO_INCREMENT,
+   idVehicle            int not null,
+   nature               varchar(254),
+   description          varchar(1500),
+   cost                 decimal(18,2),
+   dateExpenditure      datetime,
+   primary key (idExpenditure)
+);
+
+/*==============================================================*/
+/* Table: Mentainence                                           */
+/*==============================================================*/
+create table Mentainence
+(
+   idMaintennce         int not null AUTO_INCREMENT,
+   idVehicle            int not null,
+   idUser               int,
+   fault                varchar(254),
+   garage               varchar(254),
+   cost                 decimal(18,2),
+   state                varchar(254),
+   dateIn               datetime,
+   dateOut              datetime,
+   primary key (idMaintennce)
+);
+
+/*==============================================================*/
+/* Table: Rentals                                               */
+/*==============================================================*/
+create table Rentals
+(
+   idRental             int not null AUTO_INCREMENT,
+   idVehicle            int not null,
+   idDriver             int,
+   hours                int,
+   unitHour             decimal(18,2),
+   totalCost            decimal(18,2),
+   rentalType           varchar(254),
+   matriculation        varchar(254),
+   status               varchar(254),
+   created              datetime,
+   updated               datetime,
+   primary key (idRental)
+);
+
+/*==============================================================*/
+/* Table: Users                                                 */
+/*==============================================================*/
+create table Users
+(
+   idUser               int not null AUTO_INCREMENT,
+   name                 varchar(254),
+   username             varchar(254),
+   password             varchar(254),
+   role                 varchar(254),
+   matricule            varchar(254),
+   email                varchar(254),
+   phone                varchar(254),
+   picture              varchar(254),
+   status               boolean,
+   primary key (idUser)
+);
+
+/*==============================================================*/
+/* Table: Vehicles                                              */
+/*==============================================================*/
+create table Vehicles
+(
+   idVehicle            int not null AUTO_INCREMENT,
+   idModel              int not null,
+   matriculation        varchar(254),
+   seats                int,
+   colour               varchar(254),
+   technology           varchar(254),
+   availability         boolean,
+   simNumber            varchar(254),
+   feeWithDriver        decimal(18,2),
+   feeWithoutDriver     decimal(18,2),
+   created              datetime,
+   updated               datetime,
+   statut               boolean,
+   primary key (idVehicle)
+);
+
+alter table Drivers add constraint FK_association1 foreign key (idUser)
+      references Users (idUser);
+
+alter table Expenditure add constraint FK_association6 foreign key (idVehicle)
+      references Vehicles (idVehicle);
+
+alter table Mentainence add constraint FK_association5 foreign key (idVehicle)
+      references Vehicles (idVehicle);
+
+alter table Mentainence add constraint FK_association7 foreign key (idUser)
+      references Users (idUser);
+
+alter table Rentals add constraint FK_association3 foreign key (idVehicle)
+      references Vehicles (idVehicle);
+
+alter table Rentals add constraint FK_association4 foreign key (idDriver)
+      references Drivers (idDriver);
+
+alter table Vehicles add constraint FK_association2 foreign key (idModel)
+      references CarModel (idModel);
+
---
+++ autopark.sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 11, 2019 at 11:00 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `autopark`
+--
+CREATE DATABASE IF NOT EXISTS `autopark` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `autopark`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carmodel`
+--
+
+CREATE TABLE IF NOT EXISTS `carmodel` (
+  `idModel` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(254) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `mark` varchar(254) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idModel`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE IF NOT EXISTS `drivers` (
+  `idDriver` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) DEFAULT NULL,
+  `name` varchar(254) DEFAULT NULL,
+  `availability` tinyint(1) DEFAULT NULL,
+  `licenseType` varchar(254) DEFAULT NULL,
+  `licenseID` varchar(254) DEFAULT NULL,
+  `picture` varchar(254) DEFAULT NULL,
+  `idNumber` varchar(254) DEFAULT NULL,
+  `email` varchar(254) DEFAULT NULL,
+  `phone` varchar(254) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idDriver`),
+  KEY `FK_association1` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenditure`
+--
+
+CREATE TABLE IF NOT EXISTS `expenditure` (
+  `idExpenditure` int(11) NOT NULL AUTO_INCREMENT,
+  `idVehicle` int(11) NOT NULL,
+  `nature` varchar(254) DEFAULT NULL,
+  `description` varchar(1500) DEFAULT NULL,
+  `cost` decimal(18,2) DEFAULT NULL,
+  `dateExpenditure` datetime DEFAULT NULL,
+  PRIMARY KEY (`idExpenditure`),
+  KEY `FK_association6` (`idVehicle`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mentainence`
+--
+
+CREATE TABLE IF NOT EXISTS `mentainence` (
+  `idMaintennce` int(11) NOT NULL AUTO_INCREMENT,
+  `idVehicle` int(11) NOT NULL,
+  `idUser` int(11) DEFAULT NULL,
+  `fault` varchar(254) DEFAULT NULL,
+  `garage` varchar(254) DEFAULT NULL,
+  `cost` decimal(18,2) DEFAULT NULL,
+  `state` varchar(254) DEFAULT NULL,
+  `dateIn` datetime DEFAULT NULL,
+  `dateOut` datetime DEFAULT NULL,
+  PRIMARY KEY (`idMaintennce`),
+  KEY `FK_association5` (`idVehicle`),
+  KEY `FK_association7` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rentals`
+--
+
+CREATE TABLE IF NOT EXISTS `rentals` (
+  `idRental` int(11) NOT NULL AUTO_INCREMENT,
+  `idVehicle` int(11) NOT NULL,
+  `idDriver` int(11) DEFAULT NULL,
+  `hours` int(11) DEFAULT NULL,
+  `unitHour` decimal(18,2) DEFAULT NULL,
+  `totalCost` decimal(18,2) DEFAULT NULL,
+  `rentalType` varchar(254) DEFAULT NULL,
+  `matriculation` varchar(254) DEFAULT NULL,
+  `status` varchar(254) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`idRental`),
+  KEY `FK_association3` (`idVehicle`),
+  KEY `FK_association4` (`idDriver`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(254) DEFAULT NULL,
+  `username` varchar(254) DEFAULT NULL,
+  `password` varchar(254) DEFAULT NULL,
+  `role` varchar(254) DEFAULT NULL,
+  `matricule` varchar(254) DEFAULT NULL,
+  `email` varchar(254) DEFAULT NULL,
+  `phone` varchar(254) DEFAULT NULL,
+  `picture` varchar(254) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idUser`, `name`, `username`, `password`, `role`, `matricule`, `email`, `phone`, `picture`, `status`) VALUES
+(1, 'NGYIBI VADRAMA NDISANG', 'vadramson', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', 'Urs101', 'vadramson@gmail.com', '+237678583312', 'vadson.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `idVehicle` int(11) NOT NULL AUTO_INCREMENT,
+  `idModel` int(11) NOT NULL,
+  `matriculation` varchar(254) DEFAULT NULL,
+  `seats` int(11) DEFAULT NULL,
+  `colour` varchar(254) DEFAULT NULL,
+  `technology` varchar(254) DEFAULT NULL,
+  `availability` tinyint(1) DEFAULT NULL,
+  `simNumber` varchar(254) DEFAULT NULL,
+  `feeWithDriver` decimal(18,2) DEFAULT NULL,
+  `feeWithoutDriver` decimal(18,2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `statut` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idVehicle`),
+  KEY `FK_association2` (`idModel`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD CONSTRAINT `FK_association1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`);
+
+--
+-- Constraints for table `expenditure`
+--
+ALTER TABLE `expenditure`
+  ADD CONSTRAINT `FK_association6` FOREIGN KEY (`idVehicle`) REFERENCES `vehicles` (`idVehicle`);
+
+--
+-- Constraints for table `mentainence`
+--
+ALTER TABLE `mentainence`
+  ADD CONSTRAINT `FK_association7` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`),
+  ADD CONSTRAINT `FK_association5` FOREIGN KEY (`idVehicle`) REFERENCES `vehicles` (`idVehicle`);
+
+--
+-- Constraints for table `rentals`
+--
+ALTER TABLE `rentals`
+  ADD CONSTRAINT `FK_association4` FOREIGN KEY (`idDriver`) REFERENCES `drivers` (`idDriver`),
+  ADD CONSTRAINT `FK_association3` FOREIGN KEY (`idVehicle`) REFERENCES `vehicles` (`idVehicle`);
+
+--
+-- Constraints for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD CONSTRAINT `FK_association2` FOREIGN KEY (`idModel`) REFERENCES `carmodel` (`idModel`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
